41928abe60410e2adda9050ac47ccd0870c86990
Thibaut Jombart <thibautjombart@gmail.com>
2016-08-19 18:19:21 +0100

    listing recipes

---
+++ R/list.recipes.R
@@ -0,0 +1,24 @@
+#' Add recipe from yaml file
+#'
+#' This function reads a recipe described in a yaml file. Fields must contain:
+#' \itemize{
+#' \item title: the title of the recipe
+#' \item date: the date the recipe was written
+#' \item author: the author of the recipe
+#' \item email: the email of the author
+#' \item ingredients: a list of ingredients
+#' \item description: a list of instructions
+#' }
+#'
+#' @author Thibaut Jombart \email{thibautjombart@@gmail.com}
+#'
+#' @examples
+#' add.recipe("test")
+#' file.show("test.yml")
+#'
+#' @export
+#'
+list.recipes <- function(...) {
+    dir(system.file("recipes", package="eatR"), ...)
+}
+
